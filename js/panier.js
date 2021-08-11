3c141b7784dc7854ec04a18594b1fb508d06c81e
Ylan ROUSSEAU <ylan78250@gmail.com>
2021-08-11 17:12:08 +0200

    Fonction if404 et code commenter

--- js/panier.js
+++ js/panier.js
@@ -8,7 +8,7 @@ function main() {
     checkFormAndPost();
 
 }
-
+// Afficher panier vide si aucun article est dans le panier, si un ou des articles sont présent ont les affichent.
 function displayCart() {
     let basket = JSON.parse(localStorage.getItem("produit"));
     if (basket == null || basket.length == 0) {
@@ -54,14 +54,14 @@ function displayCart() {
         listeProduits.appendChild(cloneElt);
 
     }
-
+    // Bouton supprimer article individuellement 
     Array.from(document.getElementsByClassName("btn-supp")).forEach(function(element) {
         element.addEventListener('click', function(e) {
             deleteProduct(this.dataset.id);
         });
     });
 }
-
+// Affichage du prix total des éléments ce trouvant dans le panier 
 function basketTotalPrice() {
     let basket = JSON.parse(localStorage.getItem("produit"));
     let totalPrice = 0;
@@ -71,7 +71,7 @@ function basketTotalPrice() {
     document.querySelector(".order_total_amount").innerHTML = (totalPrice / 100).toFixed(2) + ' €';
 }
 
-
+// Fonction permettant de supprimer l'article désiré par rapport à son id 
 function deleteProduct(productId) {
     let basket = JSON.parse(localStorage.getItem("produit"));
     for (k = 0; k < basket.length; k++) {
@@ -90,7 +90,7 @@ function deleteProduct(productId) {
     }
 }
 
-
+// Permet de vider la panier dans son ensemble 
 function toEmptyCart() {
     let buttonToEmptyCart = document.getElementById("empty-cart");
     buttonToEmptyCart.addEventListener("click", () => {
@@ -102,7 +102,7 @@ function clearLocalStorage() {
     localStorage.clear();
     window.location.href = "panier.html";
 }
-
+// Vérifie le formulaire et envoie une réponse de l'api 
 function checkFormAndPost() {
     const order = document.getElementById("order");
     const nameRegex = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
@@ -110,7 +110,7 @@ function checkFormAndPost() {
     const adressRegex = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
     const cityRegex = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
     const checkBox = document.getElementById("invalidCheck");
-
+    // Au click le formulaire est vérifié par les regex
     order.addEventListener("click", (e) => {
         e.preventDefault();
         let inputName = document.getElementById("firstName");
@@ -118,6 +118,7 @@ function checkFormAndPost() {
         let inputCity = document.getElementById("city");
         let inputAdress = document.getElementById("address");
         let inputMail = document.getElementById("email");
+        // Lors d'un clic, si l'un des champs n'est pas rempli, ou pas correctement formaté, on affiche une erreur, on empêche l'envoi du formulaire.
         if (
             (nameRegex.test(inputName.value) == true) &
             (nameRegex.test(inputLastName.value) == true) &
@@ -126,7 +127,7 @@ function checkFormAndPost() {
             (adressRegex.test(inputAdress.value) == true) &
             (checkBox.checked == true)
         ) {
-
+            // Si le formulaire est valide, le tableau productOrder contiendra un tableau d'objet qui sont les produits acheté, et order contiendra ce tableau ainsi que l'objet qui contient les infos de l'acheteur
             let basket = JSON.parse(localStorage.getItem("produit"));
             let productOrder = [];
             for (listeId of basket) {
@@ -144,7 +145,7 @@ function checkFormAndPost() {
                 },
                 products: productOrder,
             };
-
+            // Création de l'entête de la requête POST pour l'envoyé au back-end
             const options = {
                 method: "POST",
                 body: JSON.stringify(order),
@@ -153,11 +154,11 @@ function checkFormAndPost() {
             console.log(options);
             let priceConfirmation = document.querySelector(".totalprice").innerText;
             priceConfirmation = priceConfirmation.split(" :");
-
+            // Envoie de la requête avec l'en-tête 
             fetch("http://localhost:3000/api/teddies/order", options)
                 .then((response) => response.json())
                 .then((data) => {
-                    console.log(data);
+                    // On stock les informations reçu par le back dans le local storage 
                     localStorage.setItem("orderId", data.orderId);
                     localStorage.setItem("total", priceConfirmation);
                     document.location.href = "order.html";
--- js/produit.js
+++ js/produit.js
@@ -3,11 +3,11 @@ const id = urlParams.get("id");
 basketNumb()
 main();
 
-
 function main() {
     getArticles();
+    checkIf404();
 }
-
+// On récupère uniquement le produit dont on a besoin via le paramètre dans la requête
 function getArticles() {
     const URL = `http://localhost:3000/api/teddies/` + id;
     fetch(URL)
@@ -16,9 +16,10 @@ function getArticles() {
                 let erreur = document.querySelector("#secContainer");
                 erreur.innerHTML =
                     "Nous n'avons pas réussi à afficher nos nounours.";
-                container.style.textAlign = "center";
-                container.style.padding = "35vh 0";
+                erreur.style.textAlign = "center";
+                erreur.style.padding = "35vh 0";
             })
+            // On place les données reçues via l'API aux bons endroits sur la page
             .then(function displayArticles(teddy) {
                 const productTeddy = teddy;
                 document.getElementById("productImage").src = productTeddy.imageUrl;
@@ -26,25 +27,24 @@ function getArticles() {
                 document.getElementById("productPrice").innerHTML = productTeddy.price / 100 + ',00 €';
                 document.getElementById("productDescription").innerHTML = productTeddy.description;
 
+                //Boucle pour afficher les choix de couleur du l'article 
                 for (let i = 0; i < teddy.colors.length; i++) {
                     let choix = document.createElement("option");
                     choix.innerText = teddy.colors[i];
                     productColor.appendChild(choix);
                 }
-
                 var btnAddBasket = document.querySelector("#btnAddPanier");
                 btnAddBasket.addEventListener("click", () => {
                     addProductInBasket(getBasketProduct(productTeddy), parseInt(document.getElementById("productQuantity").value));
                     document.location.reload();
                 });
             }));
-
 }
 
+
+// Création et ajout du produit dans le local storage si celui-ci est vide ou augmentation de ça quantité  
 function addProductInBasket(basketProduct, addQuantity) {
-
     let basket = JSON.parse(localStorage.getItem("produit"));
-
     if (basket == null) basket = [];
     if (isProductInBasket(basket, basketProduct.id)) {
         for (var i = 0; i < basket.length; i++) {
@@ -69,7 +69,7 @@ function isProductInBasket(basket, productId) {
     }
     return false;
 }
-
+// Création du produit qui sera ajouté au panier 
 function getBasketProduct(teddy) {
     let basketProduct = {
         id: teddy._id,
